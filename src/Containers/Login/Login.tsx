6da5625c0c70c7629f1c4e406b08cba555898cda
AntoineRb <antoine.robert7700@gmail.com>
2022-07-13 09:15:46 +0200

    remove foo component

--- src/Containers/Login/Login.tsx
+++ src/Containers/Login/Login.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Foo from '../../components/Foo/Foo';
 import './Login.scss';
 
 interface ILoginProps {
@@ -10,7 +9,6 @@ const Login: React.FunctionComponent<ILoginProps> = ( props ) => {
   return (
     <>
     <p>Login Page</p>
-    <Foo />
     </>
   );
 };
